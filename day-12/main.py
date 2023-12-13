a434fea77896f33dccec11cc4f67b72cf05085cb
Gabor Szabo <harry.bme@gmail.com>
2023-12-13 17:34:02 +0100

    Day 12 part 1 optimizations

--- day-12/main.py
+++ day-12/main.py
@@ -1,5 +1,7 @@
-from itertools import groupby
+from itertools import combinations
 from pathlib import Path
+
+import re
 
 
 def read_input(filename: str) -> list[tuple[str, list[int]]]:
@@ -11,7 +13,17 @@ def read_input(filename: str) -> list[tuple[str, list[int]]]:
 
 
 def calc_groups(record: str) -> list[int]:
-    return [len(list(group)) for char, group in groupby(record) if char == "#"]
+    result = []
+
+    counter = 0
+    for char in record:
+        if char == "#":
+            counter += 1
+        elif counter > 0:
+            result.append(counter)
+            counter = 0
+
+    return result
 
 
 def count_possible_combinations(record: str, desired_groups: list[int]) -> int:
@@ -45,6 +57,41 @@ def part_1():
     return sum
 
 
+def part_1_other():
+    # source
+    # reddit: https://www.reddit.com/r/adventofcode/comments/18ge41g/comment/kd6636r/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    # github: https://github.com/weibell/AoC2023-python/blob/main/day12/part1.py
+
+    def is_valid_arrangement(record: str, expected_group_sizes: list[int]) -> bool:
+        actual_groups = re.findall(r'#+', record)
+        actual_group_sizes = list(map(len, actual_groups))
+        return actual_group_sizes == expected_group_sizes
+
+    with open('input.txt') as f:
+        input_data = f.read().splitlines()
+
+    total_arrangements = 0
+    for line in input_data:
+        record, groups = line.split(' ')
+        group_sizes = list(map(int, groups.split(',')))
+
+        total_springs = sum(group_sizes)
+        unassigned_springs = total_springs - record.count('#')
+        unassigned_positions = [i for i, char in enumerate(record) if char == '?']
+
+        arrangements_counter = 0
+        for assignment in combinations(unassigned_positions, unassigned_springs):
+            new_record = list(record)
+            for position in assignment:
+                new_record[position] = '#'
+            if is_valid_arrangement(''.join(new_record), group_sizes):
+                arrangements_counter += 1
+
+        total_arrangements += arrangements_counter
+
+    return total_arrangements
+
+
 def part_2():
     puzzle_input = read_input("input_sample.txt")
 
@@ -59,5 +106,6 @@ def part_2():
 
 
 if __name__ == "__main__":
-    # print("Part 1:", part_1())
-    print("Part 2:", part_2())
+    print("Part 1:", part_1())
+    # print("Part 1:", part_1_other())
+    # print("Part 2:", part_2())
