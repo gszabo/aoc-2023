f5fafb469f28ac0da7c4a56ab2d58c4952d02d87
Gabor Szabo <harry.bme@gmail.com>
2023-12-15 09:33:57 +0100

    Day 12 part 2

--- day-12/main.py
+++ day-12/main.py
@@ -1,3 +1,4 @@
+from functools import cache
 from itertools import combinations
 from pathlib import Path
 
@@ -57,85 +58,104 @@ def possible_group_prefix(record: str) -> int:
     return result
 
 
-def count_arrangements(record: str, groups: list[int]) -> int:
-    # print(record, groups)
+# This is my attempt at optimizing the original `count_possible_combinations` function
+# hoping it has better cache-ability and speed.
+# For @cache to work, the groups argument has to be a tuple instead of a list,
+# because the list type is unhashable.
+@cache
+def count_arrangements(record: str, groups: tuple[int, ...]) -> int:
+    if record == "":
+        return 1 if len(groups) == 0 else 0
+
     if len(groups) == 0:
         return 1 if record.find("#") == -1 else 0
 
-    first_group, *other_groups = groups
+    first_group, other_groups = groups[0], groups[1:]
 
+    # removing .s from the beginning of the record, because they are fixed,
+    # cannot be changed and does not count into any of the groups
     stripped_record = record.lstrip(".")
 
     possible_first_group = possible_group_prefix(stripped_record)
+    possible_first_group_chars = stripped_record[:possible_first_group]
 
     if possible_first_group < first_group:
-        # print("possible_first_group < first_group")
-        return 0
+        # if the possible first group is all ? then there may still be a chance to find a good arrangement
+        # by using . for every ? in the first possible group and checking if later groups can satisfy the requirements
+        # but if there's at least one # in the possible first group, then it cannot all be ., so there has to be a
+        # group here and the size of this group cannot satisfy the requirement for the first group size
+        if "#" in possible_first_group_chars:
+            return 0
+        else:
+            return count_arrangements(stripped_record[possible_first_group:], groups)
 
     if possible_first_group == first_group:
-        # print("possible_first_group == first_group")
+        # Here first_group == possible_first_group <= len(stripped_record)
+        # because if len(stripped_record) < first_group was the case,
+        # then possible_first_group could not be equal to first_group as it cannot be longer than the string.
+
         if first_group == len(stripped_record):
             if len(groups) == 1:
-                # print("exact match")
+                # exact match
                 return 1
             else:
-                # print("exact match for first group, but there are other groups")
+                # exact match for first group, but there has to be other groups
                 return 0
         elif stripped_record[first_group] == "#":
             # too large group
-            # print("would be too large group")
             return 0
         else:
-            # print("first group done, going for next")
-            return count_arrangements(stripped_record[first_group + 1:], other_groups)
+            if "#" in possible_first_group_chars:
+                # the first group has to be the first possible group
+                return count_arrangements(
+                    stripped_record[first_group + 1 :], other_groups
+                )
+            else:
+                # one case is when the first group is the first possible group
+                # other case is when we completely jump over this possible group (all of it is ? in the else branch)
+                return count_arrangements(
+                    stripped_record[first_group + 1 :], other_groups
+                ) + count_arrangements(stripped_record[first_group:], groups)
 
     if possible_first_group > first_group:
         first_letter = stripped_record[0]
         if first_letter != "#" and first_letter != "?":
             raise ValueError(f"Unexpected first letter '{first_letter}'")
 
-        if first_group == 1:
-            second_letter = stripped_record[1]
-            if first_letter == "#":
-                if second_letter == "#":
-                    # too large group
-                    return 0
-                else:
-                    return count_arrangements(stripped_record[2:], other_groups)
+        letter_after_group = stripped_record[first_group]
+        if first_letter == "#":
+            # the group has to start here
+            if letter_after_group == "#":
+                # too large group
+                return 0
             else:
-                if second_letter == "#":
-                    # first letter has to be "." here
-                    return count_arrangements(stripped_record[2:], other_groups)
-                else:
-                    # case of ??
-                    dot_case = count_arrangements(stripped_record[1:], groups)
-                    hash_case = count_arrangements(stripped_record[2:], other_groups)
-                    return dot_case + hash_case
-        elif first_group > 1:
-            letter_after_group = stripped_record[first_group]
-            if first_letter == "#":
-                # the group has to start here
-                if letter_after_group == "#":
-                    # too large group
-                    return 0
-                else:
-                    # skip group + . char afterwards
-                    return count_arrangements(stripped_record[first_group + 1:], other_groups)
+                # skip group + ? char afterwards that has to be a separator .
+                return count_arrangements(
+                    stripped_record[first_group + 1 :], other_groups
+                )
+        elif first_letter == "?":
+            if letter_after_group == "#":
+                # first letter has to be "."
+                return count_arrangements(stripped_record[1:], groups)
             else:
                 dot_case = count_arrangements(stripped_record[1:], groups)
-                hash_case = count_arrangements(stripped_record[1:], other_groups)
+                hash_case = count_arrangements(
+                    stripped_record[first_group + 1 :], other_groups
+                )
                 return dot_case + hash_case
-        else:
-            raise ValueError(f"Invalid first group size {first_group}")
 
-    raise ValueError(f"Unexpected first group case: possible={possible_first_group} needed={first_group}")
+    raise ValueError(
+        f"Unexpected first group case: possible={possible_first_group} needed={first_group}"
+    )
 
 
 def sandbox():
     puzzle_input = read_input("input_sample.txt")
 
     for record, desired_groups in puzzle_input:
-        print(record, desired_groups, count_arrangements(record, desired_groups))
+        arrangement_count = count_arrangements(record, desired_groups)
+        # print()
+        print(record, desired_groups, arrangement_count)
 
 
 def part_1():
@@ -145,6 +165,17 @@ def part_1():
 
     for record, desired_groups in puzzle_input:
         sum += count_possible_combinations(record, desired_groups)
+
+    return sum
+
+
+def part_1_optimized_by_me():
+    puzzle_input = read_input("input.txt")
+
+    sum = 0
+
+    for record, desired_groups in puzzle_input:
+        sum += count_arrangements(record, tuple(desired_groups))
 
     return sum
 
@@ -179,26 +210,37 @@ def part_1_other():
             if is_valid_arrangement("".join(new_record), group_sizes):
                 arrangements_counter += 1
 
+        # DEBUG my algorithm, find the input where the answer is wrong.
+        # my_implementation_counter = count_arrangements(record, group_sizes)
+        # if arrangements_counter != my_implementation_counter:
+        #     print("Mismatch", record, group_sizes, arrangements_counter, my_implementation_counter)
+
         total_arrangements += arrangements_counter
 
     return total_arrangements
 
 
 def part_2():
-    puzzle_input = read_input("input_sample.txt")
+    puzzle_input = read_input("input.txt")
 
     sum = 0
 
     for record, desired_groups in puzzle_input:
         unfolded_record = "?".join([record] * 5)
         unfolded_groups = desired_groups * 5
-        sum += count_possible_combinations(unfolded_record, unfolded_groups)
+        # clearing the cache between inputs makes it faster
+        count_arrangements.cache_clear()
+        sum += count_arrangements(unfolded_record, tuple(unfolded_groups))
+        # print(count_arrangements.cache_info())
+
+
 
     return sum
 
 
 if __name__ == "__main__":
-    sandbox()
+    # sandbox()
     # print("Part 1:", part_1())
     # print("Part 1:", part_1_other())
-    # print("Part 2:", part_2())
+    print("Part 1:", part_1_optimized_by_me())
+    print("Part 2:", part_2())
